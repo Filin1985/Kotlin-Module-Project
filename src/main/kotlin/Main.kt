dea43fc168e7421974a8639ba63ba5fb72a14ea4
Marat Ikhsanov <ihsmen@gmail.com>
2024-07-07 09:27:52 +0300

    Create Archive class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,28 @@
+data class DataStorage(val name: String = "") {
+    val ARCHIVE: List<String> = listOf("Archive name", "archive name", "archive")
+}
+
+class Menu {
+    fun printMenu(
+        menuElements: MutableSet<String>,
+        archive: Archive
+    ) {
+        while(true) {
+            println("Choose or select an archive")
+            println("0 - for create the new archive")
+            menuElements.toList().forEachIndexed() {index, item -> println("${index + 1} - $item")}
+
+            println("${menuElements.size + 1} - Exit")
+
+        }
+    }
+}
+
+class Archive(val archive: MutableMap<String, MutableMap<String, String>>) {
+
+}
+
 fun main(args: Array<String>) {
+    val archive = Archive(mutableMapOf())
     println("Hello World!")
 }
